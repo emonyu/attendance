89d846fd87207be17c85f7487281beb65bab9ced
hiroa <hiroa@LAPTOP-UBQ2FUI2>
2024-07-10 12:41:17 +0900

    ログイン画面のみ

--- build/src/main/java/app/controller/MainController.java
+++ build/src/main/java/app/controller/MainController.java
@@ -1,14 +1,22 @@
 package app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	@GetMapping("/login")
-	public String getLogin() {
-		return "login";
+	public ModelAndView getLogin(ModelAndView mAndv) {
+		mAndv.setViewName("login");
+		System.out.println(passwordEncoder.encode("test"));
+		return mAndv;
 	}
 
 	@PostMapping("/login")
